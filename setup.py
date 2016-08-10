090771b7de494b69d4bd4860ca0ee8a652aefc90
Radoslav Vlaskovski <Radoslav Vlaskovski>
2016-08-10 18:06:13 +0200

    up

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='onem2mtesting',
+      version='1.0',
+      description='A Framework designed to test Applications Compliant with the oneM2M standard',
+      author='Radoslav Vlaskovski',
+      author_email='radoslav.vlaskovsky@gmail.com',
+      url='',
+      packages=find_packages(),
+      install_requires=[
+          'xmltodict',
+          'paho-mqtt',
+          'requests',
+          'python-tk'
+      ]
+     )
+
